2c58281d362c34e2c7ba7b6cf28f347af6ab9208
Anand <choudhary23.c2@gmail.com>
2020-06-11 13:55:51 +0530

    final

---
+++ subscription/subscription.js
@@ -0,0 +1,68 @@
+// function transporter(){
+//     var x =document.getElementById("trans");
+//     if (x.style.display==='none'){
+//         x.style.display='block';
+//     }
+//         else{
+//             x.style.display='none'
+//             document.getElementById('cust').style.display ='none';
+//             document.getElementById('pack').style.display ='none';
+//         }
+    
+// }
+
+// function customer(){
+//     var y =document.getElementById("cust");
+//     if (y.style.display==='none'){
+//         y.style.display='block';
+//     }
+//         else{
+//             y.style.display='none'
+//             document.getElementById('trans').style.display ='none';
+//             document.getElementById('pack').style.display ='none';
+//         }
+    
+// }
+
+// function packers(){
+//     var z =document.getElementById("pack");
+//     if (z.style.display==='none'){
+//         z.style.display='block';
+//     }
+//         else{
+//             z.style.display='none'
+//             document.getElementById('cust').style.display ='none';
+//             document.getElementById('trans').style.display ='none';
+//         }
+    
+// }
+
+// $(document).ready(function(){
+//     $("#transporter").click(function(){
+//         $("#trans").show()}),
+//         $("#customer").click(function(){
+//             $("#cust").hide()}),
+//             $("#packers").click(function(){
+//                 $("#pack").hide()});
+// })
+
+// $(document).ready(function(){
+//     $("#transporter").click(function(){
+//         $("#trans").hide()}),
+//         $("#customer").click(function(){
+//             $("#cust").show()}),
+//             $("#packers").click(function(){
+//                 $("#pack").hide()});
+// })
+
+// $(document).ready(function(){
+//     $("#transporter").click(function(){
+//         $("#trans").hide()}),
+//         $("#customer").click(function(){
+//             $("#cust").hide()}),
+//             $("#packers").click(function(){
+//                 $("#pack").show()});
+//     })
+
+
+
